2e975c02fb90657babb6d48417f51bf7a2fc25e3
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 21:00:23 +0300

    commit

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,30 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+        for (int value : functionMap.values()) {
+            if (value == requiredValue) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> functionMap = new HashMap<>();
+
+        for (int number : sourceList) {
+            int functionValue = 5 * number + 2;
+            functionMap.put(number, functionValue);
+        }
+
+        return functionMap;
     }
 }
